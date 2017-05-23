2bfbb45de626bb97123467c277e398e22a2dd6fc
Vasile Irimia <vasile.irimia@misys.com>
2017-05-23 11:23:32 +0300

    next

---
+++ timer.js
@@ -0,0 +1,14 @@
+var timer = function(name) {
+    var start = new Date();
+    return {
+        stop: function() {
+            var end  = new Date();
+            var time = end.getTime() - start.getTime();
+            console.log('Timer:', name, 'finished in', time, 'ms');
+        }
+    }
+};
+
+var t = timer('Some label');
+// code to benchmark
+t.stop(); // prints the time elapsed to the js console
